566bbc6317a54b2ac94ff546b4b0bf300ce4bee7
RT9mp <azurek0922@gmail.com>
2025-02-10 23:28:48 +0900

    이메일 인증 테스트 완료

---
+++ src/main/java/com/aivle/platform/repository/VerificationTokenRepository.java
@@ -0,0 +1,8 @@
+package com.aivle.platform.repository;
+
+import com.aivle.platform.domain.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByEmail(String email);
+}
